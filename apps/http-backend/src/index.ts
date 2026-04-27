1c0ce072088616653b10aa8f9c7de7865440fe76
KapilYadav11 <kapilyadav951641@gmail.com>
2026-04-27 20:56:54 +0530

    made the changes in the schema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,19 +3,29 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
 import { CreateRoomSchema, CreateUserSchema, SigninSchema } from '@repo/common/types';
+import { prismaClient } from '@repo/db/client';
 
 
 const app = express();
 
-app.post("signup", (req, res) =>{
+app.post("signup", async(req, res) =>{
 
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
         return res.json({
             message: "Incorrect input"
         })
         return;
     }
+
+    await prismaClient.user.create({
+        data:{
+            email: parsedData.data?.username,
+            password: parsedData.data?.password,
+            name: parsedData.data?.name
+        }
+    })
+
 
     res.json({
         userId: "123"
